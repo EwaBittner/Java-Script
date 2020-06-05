1898e4eeaa23a7d62105e68c38670248d510264b
Ewa <bittner.ewa@gmail.com>
2020-06-05 22:22:18 +0200

    Improvements added

--- js_exercise1.js
+++ js_exercise1.js
@@ -1,9 +1,16 @@
+const TAX_RATE = 0.08;
+
 var bank_account = 200;
 var price = 25;
 var purchase = 0;
+var total = 0; 
 while (bank_account > price) {
-    bank_account = bank_account - price;
-    purchase = purchase + 1;
+    price_w_tax = price + price * TAX_RATE;
+    total = total + price_w_tax;
+    bank_account = bank_account - price_w_tax;
+    purchase++;
     console.log( "You purchased an item! Your saldo is: ", bank_account );
+    console.log( "The total price with tax is: ", total);
     console.log( "You bought ", purchase, " items." );
 }
+console.log("Done");
